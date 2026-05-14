21321df7d2ab064039c7fd25d31faee378aa4f7a
Mazda Nawallsyah <mazdanawallsyah2601@gmail.com>
2026-05-14 21:21:40 +0700

    fix: add @vercel/analytics dependency and integrate Analytics component for enhanced performance monitoring

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
@@ -30,5 +31,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     />
 
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
