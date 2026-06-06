30a5a23177867677d50441e0e881cebc96307f0b
Mazda Nawallsyah <mazdanawallsyah2601@gmail.com>
2026-06-06 13:07:29 +0700

    refactor: clean up import formatting and remove outdated SEO metadata from Beranda component

--- frontend/src/components/LandingPage/Beranda.jsx
+++ frontend/src/components/LandingPage/Beranda.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import SeoHelmet from "../../components/SEOHelmet";
 
-import Hero from "../../components/LandingPage/Hero"; 
+import Hero from "../../components/LandingPage/Hero";
 
 const History = lazy(() => import("../../components/LandingPage/History"));
 const Skills = lazy(() => import("../../components/LandingPage/Skills"));
@@ -18,8 +18,6 @@ function Beranda() {
   return (
     <>
       <SeoHelmet
-        title="Mazda Nawallsyah (Newbie Front-End Developer)"
-        description="Portofolio Resmi pribadi Mazda Nawallsyah, seorang Wisudawan Universitas Semarang Prodi S1 - Teknik Informatika, yang berminat dan berfokus di Bidang Front-End Web Developer."
         url="/"
       />
 
